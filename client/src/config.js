e26151ac595c4c1678443d4c009fd9fb33baa6ea
Arturo Parrales <arturo.ps14@gmail.com>
2024-11-09 23:58:03 -0500

    Fixed env vars for react app in dockerfile

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,11 @@
 // environment config
+// react app en vars need to be prefixed by REACT_APP_
 module.exports = {
-    posts_domain: process.env.POSTS_DOMAIN || "http://localhost:4000",
-    comments_domain: process.env.COMMENTS_DOMAIN || "http://localhost:4001",
-    query_domain: process.env.QUERY_DOMAIN || "http://localhost:4002",
+    posts_domain: process.env.REACT_APP_POSTS_DOMAIN || "http://localhost:4000",
+    comments_domain: process.env.REACT_APP_COMMENTS_DOMAIN || "http://localhost:4001",
+    query_domain: process.env.REACT_APP_QUERY_DOMAIN || "http://localhost:4002",
 }
+
+console.log(`=> Posts domain - ${process.env.REACT_APP_POSTS_DOMAIN }`);
+console.log(`=> Query domain - ${process.env.REACT_APP_QUERY_DOMAIN}`);
+console.log(`=> Comments domain - ${process.env.REACT_APP_COMMENTS_DOMAIN}`);
